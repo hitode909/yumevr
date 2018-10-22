0ade6263186b9d365e6945204db38952a82c6ef0
hitode909 <hitode909@gmail.com>
2018-10-22 23:47:35 +0900

    update timing

--- a.js
+++ a.js
@@ -14,6 +14,8 @@
 
     AFRAME.registerComponent('update-html', {
         init: function () {
+            this.yumeCharacters = Array.from('夢，トイレに入るとBGMが流れる．人がいてもいなくてもBGMは流れているようだった．建物に様々なトイレがあり，それぞれに固有のBGMがある．ジャズが流れるトイレが一番気に入った．これはめっちゃいいなと思い，家でもやろうという気持ちになった．');
+            this.elementsShouldUpdate = [];
             this.canvasElements = document.querySelectorAll('.character-renderer');
             this.characterElements = document.querySelectorAll('.character-element');
             setYume = () => {
@@ -30,14 +32,21 @@
                 });
             };
             setYume();
-            setInterval(setYume, 6000);
+            // setInterval(setYume, 6000);
         },
         tick: function (t, dt) {
             this.characterElements.forEach((element, i) => {
-                const theta = (t / 3000.0 - (i / this.characterElements.length * Math.PI*2))  % (Math.PI * 2.0);
+                const theta = (t / 3000.0 - (i / this.characterElements.length * Math.PI * 2)) % (Math.PI * 2.0);
                 radius = 5.0;
-                element.object3D.position.set(-Math.cos(theta) * radius, Math.tan(theta / 2.0), -Math.sin(theta) * radius);
-                element.object3D.rotation.set(0, Math.PI/2 - theta, 0);
+                const newY = Math.tan(theta / 2.0);
+                if (element.object3D.position.y > newY) {
+                    element.components.material.shader.__targetEl.innerHTML = this.yumeCharacters[0];
+                    console.log(this.yumeCharacters[0]);
+                    this.yumeCharacters.push(this.yumeCharacters.shift());
+                    element.components.material.shader.__render();
+                }
+                element.object3D.position.set(Math.sin(theta) * radius, newY, Math.cos(theta) * radius);
+                element.object3D.rotation.set(0, theta-Math.PI, 0);
             });
         },
     });
