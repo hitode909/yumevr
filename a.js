217a7116d7dd19be5ddbf11d282cae5bef350db2
hitode909 <hitode909@gmail.com>
2018-10-23 00:02:46 +0900

    update

--- a.js
+++ a.js
@@ -1,52 +1,31 @@
 (function () {
-    (function () {
-
-    })();
-
-    const parser = new DOMParser();
-    const createElement = (source, tag) => {
-        return parser.parseFromString(source, "text/html").querySelector(tag);
-    };
-
-    const choise = (array) => {
-        return array[Math.floor(Math.random() * array.length)];
-    };
-
     AFRAME.registerComponent('update-html', {
         init: function () {
-            this.yumeCharacters = Array.from('夢，トイレに入るとBGMが流れる．人がいてもいなくてもBGMは流れているようだった．建物に様々なトイレがあり，それぞれに固有のBGMがある．ジャズが流れるトイレが一番気に入った．これはめっちゃいいなと思い，家でもやろうという気持ちになった．');
-            this.elementsShouldUpdate = [];
-            this.canvasElements = document.querySelectorAll('.character-renderer');
+            this.yumeCharacters = Array.from([
+                '夢，トイレに入るとBGMが流れる．人がいてもいなくてもBGMは流れているようだった．建物に様々なトイレがあり，それぞれに固有のBGMがある．ジャズが流れるトイレが一番気に入った．これはめっちゃいいなと思い，家でもやろうという気持ちになった．',
+                '夢，意図せず家のベランダから道に飛び降りてしまう．どうしようと思って，その場でジャンプし続けていると，重力加速度のバグで，だんだん飛距離が上がってきて，最終的に2階まで到達してベランダから家に帰れた．',
+            ].sort(() => Math.random > 0.5 ? 1 : -1).join(''));
             this.characterElements = document.querySelectorAll('.character-element');
-            setYume = () => {
-                const characters = Array.from(choise([
-                    '夢，アルミ箔みたいな日本刀で宮本武蔵と戦うことになって焦る',
-                    '速い自転車乗る，速すぎて曲るのが困難',
-                    '夢，トイレに入るとBGMが流れる．人がいてもいなくてもBGMは流れているようだった．建物に様々なトイレがあり，それぞれに固有のBGMがある．ジャズが流れるトイレが一番気に入った．これはめっちゃいいなと思い，家でもやろうという気持ちになった．',
-                ]));
-                this.canvasElements.forEach((element, i) => {
-                    element.innerHTML = characters[i % characters.length];
-                });
-                this.characterElements.forEach((element, i) => {
-                    element.components.material.shader.__render();
-                });
-            };
-            setYume();
-            // setInterval(setYume, 6000);
+
+            this.characterElements.forEach((element) => {
+                this.setYume(element.components.material.shader);
+            });
+        },
+        setYume: function (shader) {
+            shader.__targetEl.innerHTML = this.yumeCharacters[0];
+            this.yumeCharacters.push(this.yumeCharacters.shift());
+            shader.__render();
         },
         tick: function (t, dt) {
+            radius = 5.0;
             this.characterElements.forEach((element, i) => {
                 const theta = (t / 3000.0 - (i / this.characterElements.length * Math.PI * 2)) % (Math.PI * 2.0);
-                radius = 5.0;
                 const newY = Math.tan(theta / 2.0);
                 if (element.object3D.position.y > newY) {
-                    element.components.material.shader.__targetEl.innerHTML = this.yumeCharacters[0];
-                    console.log(this.yumeCharacters[0]);
-                    this.yumeCharacters.push(this.yumeCharacters.shift());
-                    element.components.material.shader.__render();
+                    this.setYume(element.components.material.shader);
                 }
                 element.object3D.position.set(Math.sin(theta) * radius, newY, Math.cos(theta) * radius);
-                element.object3D.rotation.set(0, theta-Math.PI, 0);
+                element.object3D.rotation.set(0, theta - Math.PI, 0);
             });
         },
     });
