3255b62718770304eef2dff0f2c50a938ae73a07
hitode909 <hitode909@gmail.com>
2018-10-22 23:17:12 +0900

    current status

--- a.js
+++ a.js
@@ -8,20 +8,36 @@
         return parser.parseFromString(source, "text/html").querySelector(tag);
     };
 
+    const choise = (array) => {
+        return array[Math.floor(Math.random() * array.length)];
+    };
+
     AFRAME.registerComponent('update-html', {
         init: function () {
             this.canvasElements = document.querySelectorAll('.character-renderer');
             this.characterElements = document.querySelectorAll('.character-element');
-
-            const characters = Array.from('勤怠打刻しませんか');
-            this.canvasElements.forEach((element, i) => {
-                console.log(characters[i]);
-                element.innerHTML = characters[i % characters.length];
-            });
+            setYume = () => {
+                const characters = Array.from(choise([
+                    '夢，アルミ箔みたいな日本刀で宮本武蔵と戦うことになって焦る',
+                    '速い自転車乗る，速すぎて曲るのが困難',
+                    '夢，トイレに入るとBGMが流れる．人がいてもいなくてもBGMは流れているようだった．建物に様々なトイレがあり，それぞれに固有のBGMがある．ジャズが流れるトイレが一番気に入った．これはめっちゃいいなと思い，家でもやろうという気持ちになった．',
+                ]));
+                this.canvasElements.forEach((element, i) => {
+                    element.innerHTML = characters[i % characters.length];
+                });
+                this.characterElements.forEach((element, i) => {
+                    element.components.material.shader.__render();
+                });
+            };
+            setYume();
+            setInterval(setYume, 6000);
         },
         tick: function (t, dt) {
             this.characterElements.forEach((element, i) => {
-                element.setAttribute('position', { x: -Math.cos(t / 1000 - i/3)*5, y: 2, z: -Math.sin(t / 1000 - i/3) * 5 - 4 });
+                const theta = (t / 3000.0 - (i / this.characterElements.length * Math.PI*2))  % (Math.PI * 2.0);
+                radius = 5.0;
+                element.object3D.position.set(-Math.cos(theta) * radius, Math.tan(theta / 2.0), -Math.sin(theta) * radius);
+                element.object3D.rotation.set(0, Math.PI/2 - theta, 0);
             });
         },
     });
